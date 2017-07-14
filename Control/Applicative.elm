df825f823c0731a065f5cadfc7930f71a1a37f72
Sean Innes <sean.r.innes@googlemail.com>
2017-07-14 22:17:57 +0100

    Applicatives defined

--- Control/Applicative.elm
+++ Control/Applicative.elm
@@ -1,7 +1,6 @@
 module Control.Applicative exposing (..)
 
 import Control.Functor as Functor exposing (Functor)
-import Data.Proxy exposing (Proxy(..))
 import Task
 
 
@@ -12,37 +11,37 @@ type alias AndMappable a b c d e r =
 type alias Applicative a b c d e f g h =
     AndMappable a b c d e (Functor f g h)
 
--- applicative : { b | map : a } -> c -> d -> e -> { andMap : c, ap_proxy : e, map : a, pure : d }
-applicative : (Functor f g h) -> (a -> b -> c) -> (d -> e) -> Applicative a b c d e f g h
+
+applicative : Functor f g h -> (a -> b -> c) -> (d -> e) -> Applicative a b c d e f g h
 applicative functor andMap pure =
     { map = functor.map, andMap = andMap, pure = pure }
 
 
--- list : { andMap : List (c -> d) -> List c -> List d, ap_proxy : Proxy a, map : (a1 -> b) -> List a1 -> List b, pure : a2 -> List a2 }
--- list : Applicative (List (a -> c)) (List a) (List c) a c (a -> c) (List a) (List c)
+addFunctor : { e | andMap : d -> b -> c, pure : a -> d } -> Applicative d b c a d a b c
+addFunctor rec =
+    applicative (functor rec) rec.andMap rec.pure
 
--- addFunctor : AndMappable e f g h r -> (a -> b -> c) -> (d -> b) -> Applicative a b c d h f g
--- addFunctor : { i | andMap : e -> e -> g, pure : h -> e } -> (a -> b -> c) -> (d -> b) -> Applicative a b c d h e g
 
--- addFunctor { andMap, pure } f a b = andMap (pure f) a b--andMap --applicative { map = (\f a b -> andMap (pure f) a b)}
-
-maybeMap maybe f a = maybe.andMap (maybe.pure f) a
+functor : { e | andMap : a -> b -> c, pure : d -> a } -> Functor d b c
+functor { andMap, pure } =
+    Functor (\f -> andMap (pure f))
 
 
 ziplist : Applicative (List (c -> d)) (List c) (List d) a (List a) (a1 -> b) (List a1) (List b)
 ziplist =
-    applicative Functor.list (List.map2 (<|)) (List.singleton) 
+    applicative Functor.list (List.map2 (<|)) (List.singleton)
+
 
 maybe : Applicative (Maybe (c -> d)) (Maybe c) (Maybe d) a (Maybe a) (a1 -> b) (Maybe a1) (Maybe b)
 maybe =
     applicative Functor.maybe (Maybe.map2 (<|)) Maybe.Just
 
 
--- task : { andMap : Task.Task x (c -> d) -> Task.Task x c -> Task.Task x d, ap_proxy : Proxy a, map : (a1 -> b) -> Task.Task x1 a1 -> Task.Task x1 b, pure : a2 -> Task.Task x2 a2 }
--- task =
---     applicative Functor.task (Task.map2 (<|)) Task.succeed Proxy
+task : Applicative (Task.Task x (c -> d)) (Task.Task x c) (Task.Task x d) a (Task.Task x1 a) (a1 -> b) (Task.Task x2 a1) (Task.Task x2 b)
+task =
+    applicative Functor.task (Task.map2 (<|)) Task.succeed
 
 
--- result : { andMap : Result x (b -> c) -> Result x b -> Result x c, ap_proxy : Proxy a, map : (a1 -> value) -> Result x1 a1 -> Result x1 value, pure : value1 -> Result error value1 }
--- result =
---     applicative Functor.result (Result.map2 (<|)) Result.Ok Proxy
+result : Applicative (Result x (b -> c)) (Result x b) (Result x c) value (Result error value) (a -> value1) (Result x1 a) (Result x1 value1)
+result =
+    applicative Functor.result (Result.map2 (<|)) Result.Ok
