b03333f7ea8877decb4f8619fe44e66a70751994
Sean Innes <sean.r.innes@googlemail.com>
2017-07-14 22:06:49 +0100

    Fix the definition of Applicative

--- Control/Applicative.elm
+++ Control/Applicative.elm
@@ -5,28 +5,37 @@ import Data.Proxy exposing (Proxy(..))
 import Task
 
 
-type alias AndMappable f a b c d r =
-    { r | andMap : f -> a -> b, pure : c -> a, ap_proxy : Proxy d }
+type alias AndMappable a b c d e r =
+    { r | andMap : a -> b -> c, pure : d -> e }
 
 
 type alias Applicative a b c d e f g h =
     AndMappable a b c d e (Functor f g h)
 
 -- applicative : { b | map : a } -> c -> d -> e -> { andMap : c, ap_proxy : e, map : a, pure : d }
-applicative : (Functor f g h) -> (a -> b -> c) -> (d -> b) -> Proxy e -> Applicative a b c d e f g h
-applicative functor andMap pure ap_proxy =
-    { map = functor.map, andMap = andMap, pure = pure, ap_proxy = ap_proxy }
+applicative : (Functor f g h) -> (a -> b -> c) -> (d -> e) -> Applicative a b c d e f g h
+applicative functor andMap pure =
+    { map = functor.map, andMap = andMap, pure = pure }
 
 
 -- list : { andMap : List (c -> d) -> List c -> List d, ap_proxy : Proxy a, map : (a1 -> b) -> List a1 -> List b, pure : a2 -> List a2 }
-list : Applicative (List (a -> c)) (List a) (List c) a c (a -> c) (List a) (List c)
-list =
-    applicative Functor.list (List.map2 (<|)) (List.singleton) Proxy
+-- list : Applicative (List (a -> c)) (List a) (List c) a c (a -> c) (List a) (List c)
+
+-- addFunctor : AndMappable e f g h r -> (a -> b -> c) -> (d -> b) -> Applicative a b c d h f g
+-- addFunctor : { i | andMap : e -> e -> g, pure : h -> e } -> (a -> b -> c) -> (d -> b) -> Applicative a b c d h e g
+
+-- addFunctor { andMap, pure } f a b = andMap (pure f) a b--andMap --applicative { map = (\f a b -> andMap (pure f) a b)}
+
+maybeMap maybe f a = maybe.andMap (maybe.pure f) a
 
 
--- maybe : { andMap : Maybe (c -> d) -> Maybe c -> Maybe d, ap_proxy : Proxy a, map : (a1 -> b) -> Maybe a1 -> Maybe b, pure : a2 -> Maybe a2 }
--- maybe =
---     applicative Functor.maybe (Maybe.map2 (<|)) Maybe.Just Proxy
+ziplist : Applicative (List (c -> d)) (List c) (List d) a (List a) (a1 -> b) (List a1) (List b)
+ziplist =
+    applicative Functor.list (List.map2 (<|)) (List.singleton) 
+
+maybe : Applicative (Maybe (c -> d)) (Maybe c) (Maybe d) a (Maybe a) (a1 -> b) (Maybe a1) (Maybe b)
+maybe =
+    applicative Functor.maybe (Maybe.map2 (<|)) Maybe.Just
 
 
 -- task : { andMap : Task.Task x (c -> d) -> Task.Task x c -> Task.Task x d, ap_proxy : Proxy a, map : (a1 -> b) -> Task.Task x1 a1 -> Task.Task x1 b, pure : a2 -> Task.Task x2 a2 }
